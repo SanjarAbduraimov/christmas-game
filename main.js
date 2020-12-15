b45fcffba6eb8b6f42df428b2d0989bface1acac
Sanjarbek <sanjarbekweb@gmail.com>
2020-12-16 00:04:23 +0500

    jump funtion for mobile version

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let dino = document.getElementById("dino");
 
-document.addEventListener('keypress', function (event) {
-    if (event.keyCode === 32) {
-        dino.classList.add('jump')
+// document.addEventListener('keypress', function (event) {
+//     if (event.keyCode === 32) {
+//         if (dino.classList != "jump") {
+//             dino.classList.add('jump');
+//         }
+//         setTimeout(() => {
+//             dino.classList.remove('jump');
+//         }, 800)
+//     }
+// });
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add('jump');
     }
     setTimeout(() => {
         dino.classList.remove('jump');
     }, 800)
-});
-
-
-
-
-
-
+}
 
